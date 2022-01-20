a66c6d60ec68713b49dc11d2dbb519f31e86189a
AlexSlz <pro100alecs@gmail.com>
2022-01-20 19:53:39 +0200

    add RenderExtension

---
+++ tiktokVideoApplication/tiktokVideo/FileManager.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace tiktokVideo
+{
+    internal class FileManager
+    {
+        private string path;
+        public FileManager(string _path)
+        {
+            path = _path;
+        }
+        System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
+        public string Download(string fileName, string folderName, bool CustomInterface = true)
+        {
+            watch.Start();
+            string[] data = File.ReadAllText(fileName).Split('\n');
+            if (Directory.Exists(path + @"\" + folderName))
+                Directory.Delete(path + @"\" + folderName, true);
+            Directory.CreateDirectory(path + @"\" + folderName);
+
+            List<Task> tasks = data.Select((url, index) =>
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                return client.DownloadFileTaskAsync(new Uri(url), path + $@"{folderName}\{index}.mp4"); ;
+            }).ToList();
+            if (CustomInterface)
+            {
+                DrawInterface(tasks);
+            }
+            else
+            {
+                Task.WaitAll(tasks.ToArray());
+            }
+            watch.Stop();
+            return $"All File Download, time: {watch.ElapsedMilliseconds} ms.";
+        }
+
+        private void DrawInterface(List<Task> tasks)
+        {
+            bool run = true;
+            while (run)
+            {
+                int count = 0;
+                tasks.ForEach(task => {
+                    if(!task.IsCompleted) count++;
+                });
+                run = count > 0;
+                Console.Write($"\r\t{count}/{tasks.Count}\t\r");
+                Task.WaitAny(tasks.ToArray());
+            }
+        }
+
+        public string SortFiles(string folderName, TimeSpan videoTime)
+        {
+            Console.WriteLine("Start sorting files.");
+            List<string> allFiles = Directory.GetFiles(path + folderName, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".mp4")).ToList();
+            int countVideoFolder = GetCountDirToSort(allFiles, videoTime);
+            List<List<string>> allDirectory = DivideArray(allFiles, countVideoFolder);
+            int dirCount = 0;
+            allDirectory.ForEach(dir => {
+                Directory.CreateDirectory($@"{path}{folderName}\{dirCount}");
+                dir.ForEach(file => {
+                    File.Move(file, $@"{path}{folderName}\{dirCount}\{Path.GetFileName(file)}");
+                });
+                dirCount++;
+            });
+
+            return $"All files are sorted in {countVideoFolder} folders.";
+        }
+
+        private int GetCountDirToSort(List<string> allFiles, TimeSpan videoTime)
+        {
+            var player = new WindowsMediaPlayer();
+            int countVideoFolder = 0;
+            TimeSpan allVideoTime = new TimeSpan(0);
+            allFiles.ForEach(file => {
+                allVideoTime += TimeSpan.FromSeconds(player.newMedia($"{file}").duration);
+                if (allVideoTime > videoTime)
+                {
+                    allVideoTime = TimeSpan.Zero;
+                    countVideoFolder++;
+                }
+            });
+            return (countVideoFolder == 0) ? 1 : countVideoFolder;
+        }
+
+        private List<List<string>> DivideArray(List<string> data, int size)
+        {
+            return data
+                     .Select((x, i) => new { Index = i, Value = x })
+                     .GroupBy(x => x.Index % size)
+                     .Select(x => x.Select(v => v.Value).ToList())
+                     .ToList();
+        }
+        public string Render(string folderName, string outPutFolder = "input_videos")
+        {
+            Console.WriteLine("Start render.");
+            watch.Start();
+            List<string> allDirectories = Directory.GetDirectories(path + folderName).ToList();
+            int renderIndex = 0;
+            allDirectories.ForEach(dir => {
+                SendToRenderFolder(dir, outPutFolder);
+                Console.WriteLine($"- Render #{renderIndex} Start.");
+                System.Diagnostics.Process renderProcess = System.Diagnostics.Process.Start("ffmpeg_parser.exe");
+                renderProcess.WaitForExit();
+                Console.WriteLine($"- Render #{renderIndex++} End.");
+            });
+            watch.Stop();
+            return $"All Video Render, time: {watch.ElapsedMilliseconds} ms.";
+        }
+        private void SendToRenderFolder(string dir, string outPutFolder)
+        {
+            Directory.Delete(dir, true);
+            Directory.CreateDirectory(dir);
+            List<string> allFiles = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".mp4")).ToList(); ;
+            allFiles.ForEach(file => {
+                File.Move(file, Path.Combine(Path.Combine(path.Remove(path.Length - Directory.GetParent(path).Name.Length - 1), outPutFolder, Path.GetFileName(file))));
+            });
+        }
+    }
+}
--- tiktokVideoApplication/tiktokVideo/Program.cs
+++ tiktokVideoApplication/tiktokVideo/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
-using WMPLib;
+using System.Threading.Tasks;
 
 namespace tiktokVideo
 {
@@ -10,6 +11,20 @@ namespace tiktokVideo
         static string path = AppDomain.CurrentDomain.BaseDirectory;
         static void Main(string[] args)
         {
+            //Render Extension
+            bool renderExtension = File.Exists(path + "ffmpeg_parser.exe");
+            if(renderExtension)
+            {
+                path += @"data\";
+                Directory.CreateDirectory(path);
+                Directory.CreateDirectory(path + @"\output");
+                Directory.CreateDirectory(path + @"\insert_between");
+                Directory.CreateDirectory(path + @"\input_videos");
+                Directory.CreateDirectory(path + @"\download_video");
+                if (File.Exists("logo.png"))
+                    File.Copy("logo.png", path + "logo.png");
+                path += @"\download_video\";
+            }
             if (args.Length != 0)
             {
                 foreach(string arg in args)
@@ -19,27 +34,45 @@ namespace tiktokVideo
                         string fileName = arg.Split('\\').Last();
                         string folderName = fileName.Split('.')[0];
                         //string filePath = arg.Remove(arg.Length - fileName.Length);
-                        // Код скачки/
-                        DManager dManager = new DManager(path);
-                        Console.WriteLine(dManager.Download(fileName, folderName));
-                        Console.WriteLine(dManager.SortFiles(folderName, GetTime()));
-
+                        // Download Code
+                        FileManager fManager = new FileManager(path);
+                        Console.WriteLine(fManager.Download(fileName, folderName));
+                        Console.WriteLine(fManager.SortFiles(folderName, GetTime()));
+                        if(renderExtension)
+                            Console.WriteLine(fManager.Render(folderName, "input_videos"));
                     }
                 }
             }
 
-            Thread.Sleep(3000);
-            //Console.ReadLine();
+            Thread.Sleep(1500);
+
             System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
 
         public static TimeSpan GetTime()
         {
             Console.WriteLine("Minute:");
-            int min = Int32.Parse(Console.ReadLine());
+            int min;
+            Int32.TryParse(ReadLine(), out min);
             Console.WriteLine("Second:");
-            int sec = Int32.Parse(Console.ReadLine());
+            int sec;
+            Int32.TryParse(ReadLine(), out sec);
+            if (min == 0 && sec == 0)
+                min = 10;
             return new TimeSpan(0, min, sec);
+        }
+
+        delegate string ReadLineDelegate();
+        public static string ReadLine(int timeoutms = 5000)
+        {
+            ReadLineDelegate d = Console.ReadLine;
+            IAsyncResult result = d.BeginInvoke(null, null);
+            result.AsyncWaitHandle.WaitOne(timeoutms);
+            if (result.IsCompleted)
+            {
+                return d.EndInvoke(result);
+            }
+            return "0";
         }
 
     }
